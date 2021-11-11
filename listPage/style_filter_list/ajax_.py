66d7a114eabeb926b6d6fe4a455954a948b2ff36
lyyoo <lyyoo38@gmail.com>
2021-11-11 15:37:14 +0900

    선택한 style contents의 QR code 이미지 download되도록 수정

--- listPage/style_filter_list/ajax_.py
+++ listPage/style_filter_list/ajax_.py
@@ -4,6 +4,7 @@ from django.http import JsonResponse
 
 def call_style_api(request):
     response = requests.get("http://soda-api.snow.me/v1/style/overview")
+#    response = requests.get("https://qa-soda-api.snow.me/v1/style/overview")
     return JsonResponse(response.json())
 
 def call_filter_api(request):
@@ -13,3 +14,4 @@ def call_filter_api(request):
 def call_studio_api(request):
     response = requests.get("http://soda-api.snow.me/v1/template/overview")
     return JsonResponse(response.json())
+
